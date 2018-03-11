539c88584f901c45f44844dad692f75d33f505b4
mizdra <pp.mizdra@gmail.com>
2018-03-12 01:23:14 +0900

    feat: implement tinymt32

--- src/tinymt32.rs
+++ src/tinymt32.rs
@@ -1,8 +1,89 @@
+const MIN_LOOP: i32 = 8;
+const TINYMT32_MASK: u32 = 0x7fffffff;
+const TINYMT32_SH0: u32 = 1;
+const TINYMT32_SH1: u32 = 10;
+const TINYMT32_SH8: u32 = 8;
+
 pub struct Param {
+  pub mat1: u32,
+  pub mat2: u32,
+  pub tmat: u32,
 }
 
 pub struct Rng {
+  pub param: Param,
+  pub status: [u32; 4],
 }
 
-pub fn init(param: Param, seed: u32) -> Rng {
+impl Rng {
+  pub fn next_state(&mut self) {
+    let mut x: u32;
+    let mut y: u32;
+
+    y = self.status[3];
+    x = (self.status[0] & TINYMT32_MASK)
+	    ^ self.status[1]
+	    ^ self.status[2];
+    x ^= x.wrapping_shl(TINYMT32_SH0);
+    y ^= y.wrapping_shr(TINYMT32_SH0) ^ x;
+    self.status[0] = self.status[1];
+    self.status[1] = self.status[2];
+    self.status[2] = x ^ y.wrapping_shl(TINYMT32_SH1);
+    self.status[3] = y;
+    self.status[1] ^= (-((y & 1) as i32) as u32) & self.param.mat1;
+    self.status[2] ^= (-((y & 1) as i32) as u32) & self.param.mat2;
+  }
+
+  pub fn temper(&self) -> u32 {
+    let mut t0: u32;
+    let t1: u32;
+    t0 = self.status[3];
+    t1 = self.status[0].wrapping_add(self.status[2].wrapping_shr(TINYMT32_SH8));
+    t0 ^= t1;
+    t0 ^= (-((t1 & 1) as i32) as u32) & self.param.tmat;
+    t0
+  }
+
+  pub fn gen(&mut self) -> u32 {
+    self.next_state();
+    self.temper()
+  }
+}
+
+pub fn from_seed(param: Param, seed: u32) -> Rng {
+  let mut status: [u32; 4] = [
+    seed,
+    param.mat1,
+    param.mat2,
+    param.tmat,
+  ];
+  for i in 1..MIN_LOOP {
+    let a = (i as usize) & 3;
+    let b = ((i as usize) - 1) & 3;
+    status[a] ^= (i as u32).wrapping_add(
+      1812433253u32.wrapping_mul(
+        status[b] ^ status[b].wrapping_shr(30)
+      )
+    );
+  }
+  let status: [u32; 4] = period_certification(status);
+  let mut rng = Rng { param: param, status: status };
+  rng.next_state();
+  rng
+}
+
+pub fn from_status(param: Param, status: [u32; 4]) -> Rng {
+  Rng { param: param, status: status }
+}
+
+fn period_certification(status: [u32; 4]) -> [u32; 4] {
+  if (status[0] & TINYMT32_MASK) == 0 &&
+	  status[1] == 0 &&
+	  status[2] == 0 &&
+	  status[3] == 0
+  {
+      ['T' as u32, 'I' as u32, 'N' as u32, 'Y' as u32]
+  } else {
+    status
+  }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,16 +2,31 @@ extern crate tinymt;
 
 use tinymt::tinymt32;
 
-// #[test]
-// fn tinymt32() {
-//   let mut param = tinymt32::Param {
-//     mat1: 0x8F7011EE,
-//     mat2: 0xFC78FF1F,
-//     tmat: 0x3793fdff
-//   };
-//   let rng = tinymt32::init(param, seed);
-//   // assert_eq!(rng, tinymt32::Rng { status: [0, 0, 0, 0], param: param });
+const PARAM: tinymt32::Param = tinymt32::Param {
+  mat1: 0x8F7011EE,
+  mat2: 0xFC78FF1F,
+  tmat: 0x3793fdff,
+};
+const STATUS: [u32; 4] = [0x63B07A71, 0x5740A11A, 0x3CFE1DE3, 0x08A80987];
 
-//   let v: Vec<u32> = iter::repeat(()).map(|()| rng.gen()).take(8).collect();
-//   assert_eq!(v, []);
-// }
+#[test]
+fn tinymt_should_transfer_a_next_status() {
+  let mut rng = tinymt32::from_status(PARAM, STATUS);
+
+  for _i in 0..100 {
+    rng.next_state();
+  }
+
+  assert_eq!(rng.status, [0xB2A870B4, 0x497FA995, 0xC8514463, 0x87BC580F]);
+}
+
+#[test]
+fn tinymt_should_generate_a_random_number() {
+  let mut rng = tinymt32::from_status(PARAM, STATUS);
+
+  for _i in 0..100 {
+    rng.next_state();
+  }
+
+  assert_eq!(rng.temper(), 0x34CC99F7);
+}
